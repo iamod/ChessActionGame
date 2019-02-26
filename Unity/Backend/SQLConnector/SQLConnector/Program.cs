88416cb2ec2e26565ee11b31f9d03e697520d777
Eckersleyful <45517374+Eckersleyful@users.noreply.github.com>
2019-02-26 22:31:27 +0200

    Add files via upload

---
+++ Unity/Backend/SQLConnector/SQLConnector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using ConsoleApp1;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sha1Hasher hashObj = new Sha1Hasher();
+            using (var client = new SshClient("shell1.sigmatic.fi", "shakkili", "2600_Chess_GM"))
+            {
+                string connectionStr = "server=mysql2.sigmatic.fi;user=shakkili_master;database=shakkili_avainsiirto;port=22;password=abckukko";
+                client.Connect();
+                if (client.IsConnected)
+                {
+
+                    Console.WriteLine("Connected to SSH Tunnel");
+                    var portForwarded = new ForwardedPortLocal("127.0.0.1", 3306, "127.0.0.1", 22);
+                    client.AddForwardedPort(portForwarded);
+                    portForwarded.Start();
+                    MySqlConnection conn = new MySqlConnection(connectionStr);
+
+                    try
+                    {
+                        Console.WriteLine("Connecting to mySQL DB");
+                        conn.Open();
+                        Console.WriteLine("Managed to connect to DB");
+                    }
+                    catch (Exception exc)
+                    {
+
+                        Console.WriteLine(exc.ToString());
+                    }
+
+                    ConsoleKeyInfo a = Console.ReadKey();
+                    conn.Close();
+                }
+                Console.WriteLine("Connection to SSH Tunnel failed");
+            }
+
+        }
+    }
+
+}
---
+++ Unity/Backend/SQLConnector/SQLConnector/Sha1Hasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Sha1Hasher
+    {
+        private string Salt = "sdfFasdfadf213rt35FDS5435dsf12AJHGJYdfg";
+        public string GetHash(string value)
+        {
+            var data = Encoding.ASCII.GetBytes(Salt+value);
+            var hashData = new SHA1Managed().ComputeHash(data);
+            var hash = string.Empty;
+            foreach (var b in hashData)
+            {
+                hash += b.ToString("x2");
+            }
+            return hash;
+        }
+    }
+}
